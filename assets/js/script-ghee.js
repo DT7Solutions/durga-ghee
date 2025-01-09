bab76b73073ea8df8c742aef11e3c41c9db4f7b8
meghali96 <meghaliannadasu11@gmail.com>
2025-01-09 12:09:33 +0530

    website popup

---
+++ assets/js/script-ghee.js
@@ -0,0 +1,14 @@
+// window.onload = function () {
+//     setTimeout(function () {
+//         document.getElementById("overlay").style.display = "block";
+//         document.getElementById("popup-content").style.display = "block";
+//     }, 5000); 
+// };
+
+function closePopup() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popup-content").style.display = "none";
+}
+
+
+
