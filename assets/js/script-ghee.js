77f805609f75f7f89ebc7db9acc30a9f7f21ea26
meghali96 <meghaliannadasu11@gmail.com>
2025-01-09 13:03:51 +0530

    products images amazon

--- assets/js/script-ghee.js
+++ assets/js/script-ghee.js
@@ -1,13 +1,13 @@
 // window.onload = function () {
 //     setTimeout(function () {
 //         document.getElementById("overlay").style.display = "block";
-//         document.getElementById("popup-content").style.display = "block";
+//         document.getElementById("popup").style.display = "block";
 //     }, 5000); 
 // };
 
 function closePopup() {
     document.getElementById("overlay").style.display = "none";
-    document.getElementById("popup-content").style.display = "none";
+    document.getElementById("popup").style.display = "none";
 }
 
 
